d851bb2977ca16be43d05eb951a68307e5ce4e1b
MujahidEmon <mujahidulislam.official@gmail.com>
2025-07-28 23:49:56 +0600

    learned about basic input

---
+++ Introduction to Python/input.py
@@ -0,0 +1,12 @@
+#input()  - this is input function
+#always remember - by default the value of input is always in String(Str) type
+"""
+so we need to convert the type of data according to our use case
+For example: 
+-> age_int = int(age)
+
+then it will convert to integer
+ """
+
+age = input('Enter your age: ')
+print(f"You are {age} years old")
