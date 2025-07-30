eba0a1436feb276ce84c17444e484c48d4309cd2
MujahidEmon <mujahidulislam.official@gmail.com>
2025-07-30 09:27:45 +0600

    learned about basic operators and comparision

---
+++ operators.py
@@ -0,0 +1,20 @@
+# ->   +, -, *, /, %, //(eita just purno shongkha tuk dibe point dibe na)   
+# ->   > , < , >=, <=, ==, !==
+# there is no '++' '--' in python
+
+""" 
+kono number er power ber korar syntax: 
+num ** 2   that means num square
+
+
+NB: if(condition){
+
+}
+
+python e eivabe condition use hoy na
+ """
+
+print(100/3)
+print(100%3)
+print(100//3)
+
